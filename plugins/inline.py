70fc4798a9b3ca3faea3a2746dd05621ded7022b
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2022-12-26 07:43:18 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,14 +28,14 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
-                           switch_pm_parameter="hehe")
+                           switch_pm_text='Okey Bye🚶🏻‍♂️',
+                           switch_pm_parameter="Hehe😂")
         return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to subscribe Our channel to use the bot',
                            switch_pm_parameter="subscribe")
         return
 
@@ -107,7 +107,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search Again ⟲ ', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
