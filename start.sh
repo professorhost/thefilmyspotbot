ca71db0321e89cfd727277cd6677f123fcbcac1d
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2022-12-27 12:18:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhinand2608/thefilmyspotbot /thefilmyspotbot
+  git clone https://github.com/abhinand2608/thefilmyspotbot.git /thefilmyspotbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /thefilmyspotbot
