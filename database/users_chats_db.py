7646cbb988d129d778dff212d26f193c084140d6
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2022-12-28 16:27:08 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -3,7 +3,6 @@ import motor.motor_asyncio
 
 from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER
 
-
 class Database:
     
     def __init__(self, uri, database_name):
