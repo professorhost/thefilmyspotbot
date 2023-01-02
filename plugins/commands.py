5af2caa72a92de1b4b0e6741092f2af37753f637
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2023-01-02 06:38:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -720,8 +720,8 @@ async def send_msg(bot, message):
 @Client.on_message(filters.command("deletefiles") & filters.user(ADMINS))
 async def deletemultiplefiles(bot, message):
     btn = [[
-            InlineKeyboardButton("Delete hdcam", callback_data="hdcam"),
-            InlineKeyboardButton("Delete hdcamrip", callback_data="hdcamrip")
+            InlineKeyboardButton("Delete apk", callback_data="apk"),
+            InlineKeyboardButton("Delete trailer", callback_data="trailer")
           ]]
     await message.reply_text(
         text="<b>Select the type of files you want to delete !\n\nThis will delete 100 files from the database for the selected type.</b>",
